e5891b63c3084f4b653f0b3509138577e61c09a0
James1011R <31488779+James1011R@users.noreply.github.com>
2017-08-30 18:50:29 -0400

    Update definitions.js

--- scripts/definitions.js
+++ scripts/definitions.js
@@ -947,6 +947,27 @@ MOVES = [{
     "content": "*",
     "hide": true
 }, {
+    "id": "b2",
+    "cat": "ultrapretendlimitededition",
+    "name": "clear",
+    "long": "ms]:*ally?status?deflag?&*ally?enchant?deflag?&deflag",
+    "text": "Clear unit and location, removing all status effects and location effects.",
+    "color": [220, 220, 220],
+    "color2": [255, 255, 255],
+    "color3": [0, 0, 0],
+    "content": "\u2300",
+    "hide": true
+}, {
+    "id": "b3",
+    "cat": "ultrapretendlimitededition",
+    "name": "decimate",
+    "long": "c]ru:set@value=-3",
+    "text": "(Magic) Target loses 3 value. If unit has 3 value or less, destroy it instead.",
+    "color": [64, 0, 255],
+    "color2": [0, 0, 0],
+    "content": "\uFE63",
+    "hide": true
+}, {
     "id": "c1",
     "cat": "custom",
     "name": "custom1",
@@ -1018,6 +1039,7 @@ MOVES = [{
     "text": "Custom action 9 (Double Click Menu Icon to Edit)",
     "color": [25, 25, 227],
     "content": "9"
+    "hide": true
 }, {
     "id": "c10",
     "cat": "custom",
@@ -1026,6 +1048,7 @@ MOVES = [{
     "text": "Custom action 10 (Double Click Menu Icon to Edit)",
     "color": [126, 25, 227],
     "content": "A"
+    "hide": true
 }, {
     "id": "c11",
     "cat": "custom",
@@ -1034,6 +1057,7 @@ MOVES = [{
     "text": "Custom action 11 (Double Click Menu Icon to Edit)",
     "color": [227, 25, 227],
     "content": "B"
+    "hide": true
 }, {
     "id": "c12",
     "cat": "custom",
@@ -1042,6 +1066,7 @@ MOVES = [{
     "text": "Custom action 12 (Double Click Menu Icon to Edit)",
     "color": [227, 25, 126],
     "content": "C"
+    "hide": true
 }, {
     "id": "c13",
     "cat": "custom",
@@ -1050,6 +1075,7 @@ MOVES = [{
     "text": "Custom action 13 (Double Click Menu Icon to Edit)",
     "color": [25, 25, 25],
     "content": "D"
+    "hide": true
 }, {
     "id": "c14",
     "cat": "custom",
@@ -1058,6 +1084,7 @@ MOVES = [{
     "text": "Custom action 14 (Double Click Menu Icon to Edit)",
     "color": [126, 126, 126],
     "content": "E"
+    "hide": true
 }, {
     "id": "c15",
     "cat": "custom",
@@ -1066,6 +1093,7 @@ MOVES = [{
     "text": "Custom action 15 (Double Click Menu Icon to Edit)",
     "color": [227, 227, 227],
     "content": "F"
+    "hide": true
 }, {
     "id": "c16",
     "cat": "custom",
@@ -1074,6 +1102,7 @@ MOVES = [{
     "text": "Custom action 16 (Double Click Menu Icon to Edit)",
     "color": [126, 126, 25],
     "content": "0"
+    "hide": true
 }];
 
 /* Self reminder
